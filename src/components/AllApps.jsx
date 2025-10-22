de3b22828620a8c7c80ac6a41b3277a2bd951ae7
oliullahakib <oliullahakib@gmail.com>
2025-10-22 19:42:38 +0600

    dynamic title added for all apps page

--- src/components/AllApps.jsx
+++ src/components/AllApps.jsx
@@ -7,6 +7,7 @@ const AllApps = () => {
     // console.log(gameData)
     return (
         <div >
+            <title>Game Hub - All Apps</title>
            <h2 className='text-xl font-semibold my-8'>All Games({gameData.length})</h2>
            <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 px-3'>
             {

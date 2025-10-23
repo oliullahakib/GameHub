c369ee4eb04b23b410d5fea3067aad312be9cc73
oliullahakib <oliullahakib@gmail.com>
2025-10-23 14:01:08 +0600

    google login functionality and dynamic title added to login and register page

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, updateProfile } from 'firebase/auth';
 import { auth } from '../firebase/firebase.config';
 
 const AuthProvider = ({children}) => {
+    const googleProvider = new GoogleAuthProvider()
     const creatUser = (email,password)=>{
         return createUserWithEmailAndPassword(auth,email,password);
     };
@@ -13,10 +14,15 @@ const AuthProvider = ({children}) => {
     const updateUser = (user,userObj)=>{
         return updateProfile(user,userObj);
     }
+    const googleLogin =()=>{
+        return signInWithPopup(auth,googleProvider);
+    }
+    
     const value = {
         creatUser,
         loginUser,
-        updateUser
+        updateUser,
+        googleLogin
     }
     return <AuthContext value={value}> {children}</AuthContext>
 };
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 import { AuthContext } from '../Context/AuthContext';
 
 const Login = () => {
-    const { loginUser } = use(AuthContext)
+    const { loginUser,googleLogin } = use(AuthContext)
     const [show, setShow] = useState(false)
     const handleLogin = (e) => {
         e.preventDefault();
@@ -20,10 +20,20 @@ const Login = () => {
                 const errorCode = error.code;
                console.log("login",errorCode);
             });
-    
+    }
+     const handleGoogleLogin = () => {
+        googleLogin()
+            .then((result) => {
+                const user = result.user;
+                console.log(user)
+            }).catch((error) => {
+                const errorCode = error.code;
+                console.log(errorCode)
+            });
     }
     return (
         <div className="hero min-h-screen">
+             <title>Game Hub - Login</title>
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="card bg-[#2F3645] w-full min-w-96 shrink-0 shadow-2xl">
                     <div className="card-body">
@@ -52,7 +62,7 @@ const Login = () => {
                                 <div className='w-1/2 h-px bg-base-200'></div>
                             </div>
                             {/* Google */}
-                            <button className="btn mb-2 bg-white text-black border-[#e5e5e5]">
+                            <button onClick={handleGoogleLogin} className="btn mb-2 bg-white text-black border-[#e5e5e5]">
                                 <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                                 Login with Google
                             </button>
